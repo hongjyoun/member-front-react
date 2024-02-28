5d54ab1031fa12ecbe7799c72f3c9acdb9638cb9
hongjyoun <hongjyoun@nate.com>
2024-02-28 17:07:20 +0900

    background setted

--- @apps/admin/src/theme/index.jsx
+++ @apps/admin/src/theme/index.jsx
@@ -1,6 +1,11 @@
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
+    palette: {
+        background: {
+            default: '#F0F0F0',
+        },
+    },
     breakpoints: {
         values: {
             xs: 0,
