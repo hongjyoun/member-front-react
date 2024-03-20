48c90ba02dc983f6e4150ab099434de608ba7512
hongjyoun <hongjyoun@nate.com>
2024-03-20 14:00:10 +0900

    setTableParams 적용

--- @apps/admin/src/pages/Tables.jsx
+++ @apps/admin/src/pages/Tables.jsx
@@ -5,28 +5,30 @@ import NameInput from '@/components/common/NameInput';
 
 const Tables = () => {
   const [ name, setName ] = useState('');
+  const [tableParams, setTableParams] = useState({
+    pagination: { current: 1, pageSize: 10, showSizeChanger: true, pageSizeOptions: [5,10,20,30] },
+  });
   const { data, isLoading } = useGetBySearchParams();
-  console.log(data ? data.data : 'no data');
 
   const columns = [
     { title: 'ID', dataIndex: 'id', key: 'id' },
-    { title: 'Name', dataIndex: 'firstName', key: 'name', render: (firstName) => <a>{firstName}</a> },
-    { title: '센터', dataIndex: 'center', key: 'center', render: (center) => <a>{center?.name}</a> },
-    { title: '현재과정', dataIndex: 'levelProgress', key: 'level', render: (lp) => <a>{lp?.level?.name}</a> },
+    { title: 'Name', dataIndex: 'firstName', key: 'name' },
+    { title: '센터', dataIndex: 'center', key: 'center', render: (center) => <div>{center?.name}</div> },
+    { title: '현재과정', dataIndex: 'levelProgress', key: 'level', render: (lp) => <div>{lp?.level?.name}</div> },
   ];
 
-  const rows = data ? data.data : [];
+  const dataSource = data ? data.data : [];
 
-  const [tableParams, setTableParams] = useState({
-    pagination: {
-      current: 1,
-      pageSize: 20,
-    },
-  });
+
 
   const handleName = (e) => {
     e.preventDefault();
     setName(e.target.value);
+  }
+
+  const handleOnChange = (pagination, filters, sorter, extra) => {
+    console.log(pagination, filters, sorter, extra);
+    setTableParams({ pagination });
   }
 
   return (
@@ -34,7 +36,11 @@ const Tables = () => {
       <h1>Tables</h1>
       <span>{name}</span>
       <NameInput placeholder="이름을 입력하세요" name={name} setName={handleName}/>
-      <Table columns={columns} dataSource={rows} pagination={tableParams.pagination} rowKey="id"/>
+      { isLoading
+          ? <div>로딩중...</div>
+          : <Table columns={columns} dataSource={dataSource} pagination={tableParams.pagination} rowKey="id" onChange={handleOnChange}/>
+
+      }
     </>
   );
 };
