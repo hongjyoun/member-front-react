69ccd6a8db44f67b72c1775064b61599312190d2
hongjyoun <hongjyoun@nate.com>
2024-05-21 11:16:56 +0900

    navigation changed

--- @apps/admin/src/App.jsx
+++ @apps/admin/src/App.jsx
@@ -1,10 +1,8 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { CssBaseline, ThemeProvider } from '@mui/material';
 import AppRouter from './routes/AppRouter';
-import theme from './theme';
-import GlobalStyles from './theme/GlobalStyles';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { ModalProvider } from '@/plugins/modal/ModalProvider';
+import '@/assets/main.less';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,13 +16,9 @@ const queryClient = new QueryClient({
 const App = () => {
   return <QueryClientProvider client={queryClient}>
     <ReactQueryDevtools initialIsOpen={false} />
-    <ThemeProvider theme={theme}>
-      <GlobalStyles/>
-      <CssBaseline/>
       <ModalProvider>
         <AppRouter/>
       </ModalProvider>
-    </ThemeProvider>
   </QueryClientProvider>;
 };
 
---
+++ @apps/admin/src/components/menu/SideBarOld.jsx
@@ -0,0 +1,45 @@
+import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import KeycloakService from '@/module/keycloak';
+import './SideBarOld.less';
+
+const SideBarOld = () => {
+  const authService = new KeycloakService();
+  const onclickLogin = () => { authService.login(); };
+  const onclickLogout = async () => { await authService.logout(); };
+
+  return <>
+    <Box sx={{ width: '100%', maxWidth: 230, bgcolor: 'white', height: '100vh' }}>
+      <div className="navWrap">
+        <nav aria-label="main mailbox folders">
+          <List>
+            <ListItem disablePadding>
+              <ListItemButton component={Link} to="/home">
+                <ListItemText primary="Home"/>
+              </ListItemButton>
+            </ListItem>
+            {
+              authService.isAuthenticated() &&
+              <ListItem disablePadding>
+                <ListItemButton component={Link} to="/tables">
+                  <ListItemText primary="Tables"/>
+                </ListItemButton>
+              </ListItem>
+            }
+          </List>
+        </nav>
+        <div className="loginButton">
+          {!authService.isAuthenticated() && <Button onClick={onclickLogin} variant={'outlined'} fullWidth={true}>로그인</Button>}
+          {authService.isAuthenticated() && <Button onClick={onclickLogout} variant={'outlined'} fullWidth={true}>로그아웃</Button>}
+        </div>
+      </div>
+    </Box>
+  </>;
+};
+
+export default SideBarOld;
